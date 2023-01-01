9d3ff5f9de7549379201699cc4aa0141d1286c71
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2023-01-02 00:01:36 +0530

    01 JSX : converting html to jsx

--- 01_jsx/src/index.js
+++ 01_jsx/src/index.js
@@ -20,14 +20,20 @@ function App() {
 
     // return <h1> {time} </h1>;
     // return <h1>{ new Date().toLocaleTimeString() }</h1>
-    const name = "Sahil";
-    const age = 25;
-    return (
-        <h1>
-            Hi, my name is {name} and my age is {age}!
-        </h1>
-    );
-
+    // const name = "Sahil";
+    // const age = 25;
+    // return (
+    //     <h1>
+    //         Hi, my name is {name} and my age is {age}!
+    //     </h1>
+    // );
+    // return <input type="number" min={5} max={10}/>
+    // const inputType = 'number';
+    // const minValue = 5;
+    //
+    // return <input type={inputType} min={minValue}/>
+    // return <input style={{ border: '3px solid red' }} type="number"/>
+    return <textarea autoFocus={true} />
 }
 
 // 5. Show the component on the screen
