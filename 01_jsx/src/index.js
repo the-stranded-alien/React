72b00c18c0ed3cdb6f3040fd089ad49a8c658bf4
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2023-01-01 18:37:48 +0530

    01 JSX : jsx basics

--- 01_jsx/src/index.js
+++ 01_jsx/src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// 1. Import the React and ReactDOM Libraries
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// 2. Get a reference to the div with ID root
+const el = document.getElementById('root');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 3. Tell React to take control of that element
+const root = ReactDOM.createRoot(el);
+
+// 4. Create a component
+function App() {
+    let message = 'Bye There !';
+    if (Math.random() > 0.5) {
+        message = 'Hello There !';
+    }
+
+    const date = new Date();
+    const time = date.toLocaleTimeString();
+
+    // return <h1> {time} </h1>;
+    // return <h1>{ new Date().toLocaleTimeString() }</h1>
+    const name = "Sahil";
+    const age = 25;
+    return (
+        <h1>
+            Hi, my name is {name} and my age is {age}!
+        </h1>
+    );
+
+}
+
+// 5. Show the component on the screen
+root.render(<App />);
