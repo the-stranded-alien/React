174eec5f1350c660b272c853cc86412c6a0863d4
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2023-01-05 17:42:39 +0530

    03 State : State System

--- 03_state/src/App.js
+++ 03_state/src/App.js
@@ -1,12 +1,30 @@
+import "./App.css";
+import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+
+function getRandomAnimal() {
+    const animals = ['cat', 'bird', 'cow', 'dog', 'gator', 'horse'];
+    return animals[Math.floor(Math.random() * animals.length)];
+}
+
 function App() {
 
+    const [animals, setAnimals] = useState([]);
+
     const handleClick = () => {
-        console.log('Button was clicked!')
-    }
+        setAnimals([...animals, getRandomAnimal()])
+    };
+
+    const renderedAnimals = animals.map((animal, index) => {
+        return <AnimalShow type={animal} key={index} />
+    });
 
     return (
-        <div>
+        <div className="app">
             <button onClick={handleClick}> Add Animal </button>
+            <div className="animal-list">
+                { renderedAnimals }
+            </div>
         </div>
     );
 }
