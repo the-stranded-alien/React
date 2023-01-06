9c6e621e971606d369a01401b596467f6ec7b1c9
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2023-01-07 02:15:06 +0530

    05 Form: Handling Forms

--- 05_form/src/index.js
+++ 05_form/src/index.js
@@ -1,7 +1,9 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const ele = document.getElementById('root');
 const root = ReactDOM.createRoot(ele);
+
 root.render(<App />);
