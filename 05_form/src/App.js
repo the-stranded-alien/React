5e6da495a507ed29c096e8e4937d315e0fb1e20d
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2023-01-07 17:33:21 +0530

    05 Forms: Persisting Data and UseEffect

--- 05_form/src/App.js
+++ 05_form/src/App.js
@@ -1,29 +1,53 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
+import axios from "axios";
 
 function App() {
   const [books, setBooks] = useState([]);
 
-  const createBook = (title) => {
-      const updatedBooks = [
-          ...books,
-          { id: Math.round(Math.random() * 9999), title }
-      ];
-      setBooks(updatedBooks);
+  const fetchBooks = async () => {
+    const response = await axios.get("http://localhost:3001/books");
+    setBooks(response.data);
   };
 
-  const editBookById = (id, newTitle) => {
+  useEffect(() => {
+      fetchBooks();
+  } , []); // Called only on initial render
+
+  const createBook = async (title) => {
+
+      const response = await axios.post('http://localhost:3001/books', {
+          title
+      });
+
+      const updatedBooks = [ ...books, response.data ];
+      setBooks(updatedBooks);
+
+      // Without Data Persistence
+      // const updatedBooks = [
+      //     ...books,
+      //     { id: Math.round(Math.random() * 9999), title }
+      // ];
+      // setBooks(updatedBooks);
+  };
+
+  const editBookById = async (id, newTitle) => {
+      const response = await axios.put(`http://localhost:3001/books/${id}`, {
+          title: newTitle
+      });
+
       const updatedBooks = books.map((book) => {
           if(book.id === id) {
-              return { ...book, title: newTitle};
+              return { ...book, ...response.data };
           }
           return book;
       });
       setBooks(updatedBooks);
   };
 
-  const deleteBookById = (id) => {
+  const deleteBookById = async (id) => {
+      await axios.delete(`http://localhost:3001/books/${id}`);
       const updatedBooks = books.filter((book) => {
           return book.id !== id;
       });
