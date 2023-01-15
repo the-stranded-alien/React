ccf694c5c2704040a70d31c3f3fea1d35536c19b
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2023-01-15 23:01:28 +0530

    07 Routing: Custom Navigation

--- 07_routing/src/App.js
+++ 07_routing/src/App.js
@@ -1,25 +1,23 @@
-import { useState } from "react";
-import Dropdown from "./components/Dropdown";
-
-
+import Sidebar from "./components/Sidebar";
+import Route from "./components/Route";
+import AccordionPage from "./pages/AccordionPage";
+import ButtonPage from "./pages/ButtonPage";
+import DropdownPage from "./pages/DropdownPage";
 function App() {
-    const [selection, setSelection] = useState(null);
-
-    const handleSelection = (option) => {
-        setSelection(option);
-    }
-
-    const options = [
-        { label: 'Red', value: 'red' },
-        { label: 'Green', value: 'green' },
-        { label: 'Blue', value: 'blue' },
-    ];
-
-    return (
-        <div className="flex">
-            <Dropdown options={options} value={selection} onChange={handleSelection}/>
+    return (<div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
+        <Sidebar />
+        <div className="col-span-5">
+            <Route path="/">
+                <DropdownPage />
+            </Route>
+            <Route path="/accordion">
+                <AccordionPage />
+            </Route>
+            <Route path="/buttons">
+                <ButtonPage />
+            </Route>
         </div>
-    );
+    </div>);
 }
 
 export default App;
--- 07_routing/src/index.js
+++ 07_routing/src/index.js
@@ -2,10 +2,13 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {NavigationProvider} from "./context/navigation";
 
 const ele = document.getElementById('root');
 const root = ReactDOM.createRoot(ele);
 
 root.render(
-    <App />
+    <NavigationProvider>
+        <App />
+    </NavigationProvider>
 );
