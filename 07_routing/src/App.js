ecb158499fbe90a7a2fd90bb76daedebd6e7a502
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2023-01-16 19:02:27 +0530

    07 Routing: Custom Hooks

--- 07_routing/src/App.js
+++ 07_routing/src/App.js
@@ -5,6 +5,7 @@ import ButtonPage from "./pages/ButtonPage";
 import DropdownPage from "./pages/DropdownPage";
 import ModalPage from "./pages/ModalPage";
 import TablePage from "./pages/TablePage";
+import CounterPage from "./pages/CounterPage";
 function App() {
     return (<div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
         <Sidebar />
@@ -23,6 +24,9 @@ function App() {
             </Route>
             <Route path="/table">
                 <TablePage />
+            </Route>
+            <Route path="/counter">
+                <CounterPage initialCount={10} />
             </Route>
         </div>
     </div>);
