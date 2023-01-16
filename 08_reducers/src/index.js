0af6c879a48c77151e0d1ee12e3de8cc84167ff0
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2023-01-16 19:07:15 +0530

    08 Reducers: Source Code

---
+++ 08_reducers/src/index.js
@@ -0,0 +1,14 @@
+import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import {NavigationProvider} from "./context/navigation";
+
+const ele = document.getElementById('root');
+const root = ReactDOM.createRoot(ele);
+
+root.render(
+    <NavigationProvider>
+        <App />
+    </NavigationProvider>
+);
