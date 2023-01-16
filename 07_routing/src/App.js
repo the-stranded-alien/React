77a1faacd84380d7e33d893f1fa58b5a970f74a3
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2023-01-16 15:24:10 +0530

    07 Routing: Data Table

--- 07_routing/src/App.js
+++ 07_routing/src/App.js
@@ -4,6 +4,7 @@ import AccordionPage from "./pages/AccordionPage";
 import ButtonPage from "./pages/ButtonPage";
 import DropdownPage from "./pages/DropdownPage";
 import ModalPage from "./pages/ModalPage";
+import TablePage from "./pages/TablePage";
 function App() {
     return (<div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
         <Sidebar />
@@ -19,6 +20,9 @@ function App() {
             </Route>
             <Route path="/modal">
                 <ModalPage />
+            </Route>
+            <Route path="/table">
+                <TablePage />
             </Route>
         </div>
     </div>);
