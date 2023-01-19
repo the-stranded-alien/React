17fb1eed9bc19a4c413b6654facf7444737feb6e
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2023-01-19 20:56:58 +0530

    10 Multiple Slices: Source Code

---
+++ 10_multiple_slices_rtk/src/App.js
@@ -0,0 +1,17 @@
+import CarForm from "./components/CarForm";
+import CarSearch from "./components/CarSearch";
+import CarList from "./components/CarList";
+import CarValue from "./components/CarValue";
+
+function App() {
+    return (
+        <div>
+            <CarForm />
+            <CarList />
+            <CarSearch />
+            <CarValue />
+        </div>
+    );
+}
+
+export default App;
