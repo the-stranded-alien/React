f32c15e13c24f1175f0813b7bd1f4d96d2d4830f
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2023-01-20 14:23:06 +0530

    10 Multiple Slices Redux Toolkit

--- 10_multiple_slices_rtk/src/components/CarValue.js
+++ 10_multiple_slices_rtk/src/components/CarValue.js
@@ -1,5 +1,31 @@
+import { useSelector } from "react-redux";
+
 function CarValue() {
-    return <div> Car Value !</div>
+
+    const totalCost = useSelector(({ cars: { carsList, searchTerm }}) =>
+        // const filteredCars = carsList.filter((car) => {
+        //     return car.name.toLowerCase().includes(searchTerm.toLowerCase());
+        // });
+        // let cost = 0;
+        // for(let car of filteredCars) {
+        //     cost += car.cost;
+        // }
+        // return cost;
+        // return filteredCars.reduce((acc, car) => {
+        //     return acc + car.cost;
+        // }, 0);
+        // return filteredCars.reduce((acc, car) => acc + car.cost, 0);
+
+        carsList
+            .filter((car) => car.name.toLowerCase().includes(searchTerm.toLowerCase()))
+            .reduce((acc, car) => acc + car.cost, 0)
+    );
+
+    return (
+        <div className="car-value">
+            Total Cost: ${totalCost}
+        </div>
+    );
 }
 
 export default CarValue;
--- 10_multiple_slices_rtk/src/index.js
+++ 10_multiple_slices_rtk/src/index.js
@@ -1,9 +1,14 @@
+import 'bulma/css/bulma.css';
+import './style.css';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from "react-redux";
+import { store } from "./store";
 import App from './App';
-
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
