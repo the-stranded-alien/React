4860403326e5d242a348a99657324d12b4beb4c3
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2023-01-21 13:29:04 +0530

    11 Async Thunks: RTK Query

--- 11_async_thunks/src/components/AlbumsList.js
+++ 11_async_thunks/src/components/AlbumsList.js
@@ -1,6 +1,37 @@
+import { useFetchAlbumsQuery, useAddAlbumMutation } from "../store";
+import Skeleton from "./Skeleton";
+import Button from "./Button";
+import AlbumsListItem from "./AlbumsListItem";
+
 function AlbumsList({ user }) {
+    const { data, error, isFetching } = useFetchAlbumsQuery(user);
+    const [addAlbum, results] = useAddAlbumMutation();
+
+    const handleClick = () => {
+        addAlbum(user);
+    }
+
+    let content;
+    if (isFetching) {
+        content = <Skeleton times={3} className="h-10 w-full" />
+    } else if (error) {
+        content = <div> Error Loading Albums ! </div>
+    } else {
+        content = data.map((album) => {
+            return <AlbumsListItem key={album.id} album={album} />
+        });
+    }
+
     return (
-        <div> Albums for {user.name} </div>
+        <div>
+            <div className="m-2 flex flex-row items-center justify-between">
+                <h3 className="text-lg font-bold">Albums by {user.name}</h3>
+                <Button loading={ results.isLoading } onClick={handleClick}>
+                    + Add Album
+                </Button>
+            </div>
+            <div> {content} </div>
+        </div>
     );
 }
 
